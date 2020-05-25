4aa05a9ca1410bf5daf1519dc0854f4aa592b286
Rizal Fakhri <me@rizalfakhri.id>
2020-05-26 03:52:24 +0800

    Add Banner & chanfe to default port 80

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo "WELCOME"
+function Banner()
+{
+    echo -e "\e[91m #########################################################################\e[0m"
+    echo -e "   \e[95m_______\e[0m  \e[95m_______\e[0m  __   __  _______  _______  __    _  _______  ___       "
+    echo -e "  |       ||       ||  | |  ||       ||   \e[91m_\e[0m   ||  |  | ||       ||   |      "
+    echo -e "  |  _____||  _____||  |_|  ||    \e[91m_\e[0m  ||  \e[91m|_|\e[0m  ||  |_|  ||    ___||   |      "
+    echo -e "  | |_____ | |_____ |       ||   \e[91m|_|\e[0m ||       ||       ||   |___ |   |      "
+    echo -e "  |_____  ||_____  ||       ||    ___||       ||  _    ||    ___||   |___   "
+    echo -e "   _____| | _____| ||   _   ||   |    |   _   || | |   ||   |___ |       |  "
+    echo -e "  |\e[95m_______\e[0m||\e[95m_______\e[0m||__| |__||___|    |__| |__||_|  |__||_______||_______|  "
+    echo ""
+    echo -e "\e[91m #########################################################################\e[0m"
+
+    echo "SSHPANEL™ SOFTETHER PANEL INSTALLATION"
+    echo "~ Version: 1.0"
+}
+
+Banner
 
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
@@ -9,4 +26,4 @@ docker-compose up -d
 
 docker exec app bash init
 
-echo "DONE!"
+echo "INSTALLATION COMPLETED!"
